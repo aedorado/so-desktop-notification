616393fc3242500c8d8791bd6d14fffa5a431b65
Anurag El Dorado <annurag94@gmail.com>
2016-02-23 21:17:02 +0530

    Added comments.

--- App.py
+++ App.py
@@ -9,6 +9,8 @@ class App():
 
     def __init__(self, labeltext):
 
+        self.url = labeltext[labeltext.find('\n') + 1:]
+
         self.root = Tk()
         self.root.overrideredirect(1)
         self.root.configure(background=self.bgColor)    # setting background color
@@ -21,24 +23,23 @@ class App():
         self.root.geometry('%dx%d+%d+%d' % (self.w, self.h, ws - self.w - 25, 40))
 
         self.label = Label(self.root, text=labeltext, fg='#ffffff', bg=self.bgColor, wraplength=350)
-        self.label.pack(pady=20)
+        self.label.pack(pady=20)    # put label on root
+        self.label.bind("<Button-1>", self.bopen)   # bind bopen function on label
 
         self.bQuit = Button(self.root, text="X", command=self.closeSelf)
-        self.bQuit.place(x=375, y=0, height=25, width=25)
-        self.bQuit.configure(background = '#77216F')
+        self.bQuit.configure(background = '#77216F')    # configure background of the button
+        self.bQuit.place(x=375, y=0, height=25, width=25)   # place the quit button
 
-        self.label.bind("<Button-1>", self.bopen)
         
-        self.root.after(7500, lambda: self.root.destroy())
-        self.url = labeltext[labeltext.find('\n') + 1:]
+        self.root.after(7500, lambda: self.root.destroy())  # close popup after 7500 second itself
         self.root.lift()
-        self.root.call('wm', 'attributes', '.', '-topmost', True)
+        self.root.call('wm', 'attributes', '.', '-topmost', True)   # so that window is always topmost
         self.root.mainloop()
 
-    def closeSelf(self):
+    def closeSelf(self):        # closes the popup
         self.root.destroy()
 
-    def bopen(self, event):
+    def bopen(self, event):     # open the url in default webbrowser
         webbrowser.open(self.url)
         self.closeSelf()
 
